aaab958dfbc90d30f4f917f9f2c7d3b32cbb05f5
maxoreau <maxime.horeau@gmail.com>
2017-06-27 14:09:25 +0200

    first commit

---
+++ src/main/java/org/maxoreau/MaxBatailleNavale/MaxBatailleNavaleApplication.java
@@ -0,0 +1,12 @@
+package org.maxoreau.MaxBatailleNavale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxBatailleNavaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxBatailleNavaleApplication.class, args);
+	}
+}
